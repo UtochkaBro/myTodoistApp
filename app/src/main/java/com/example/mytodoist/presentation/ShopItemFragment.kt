54f817246935b34b5d0cceb752e6b3138feb1e33
Vyacheslav Nikitin <space.face.ru@gmail.com>
2022-07-24 16:07:24 +0300

    5.2 setup fragment

---
+++ app/src/main/java/com/example/mytodoist/presentation/ShopItemFragment.kt
@@ -0,0 +1,17 @@
+package com.example.mytodoist.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.mytodoist.R
+
+class ShopItemFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_shop_item, container, false)
+    }
+}
